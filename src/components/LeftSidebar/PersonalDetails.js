47838018e2e0fde45431dde8500e6ab7e55eec94
programmerNizam <mdnizam12109@gmail.com>
2024-02-13 00:51:07 +0600

    [Added] Developer Details Profile Avater design

--- src/components/LeftSidebar/PersonalDetails.js
+++ src/components/LeftSidebar/PersonalDetails.js
@@ -1,16 +1,31 @@
-import person from '@/assets/person.png';
+import person from '@/assets/profile.jpg';
 import Image from "next/image";
 
 const devInfo = {
-    name: 'MD Nizam Uddin'
+    name: 'MD Nizam Uddin',
+    designation: 'Fronted Developer',
+    fbLink: '#',
+    twLink: '#',
+    ldLink: '#',
+    igLink: '#',
+    ghLink: '#',
+    email: "info.mdnizamuddin@gmail.com",
+    phone: '+880 163 8548 900',
+    country: 'Bangladesh',
+    city: 'Chandpur',
+    age: '22',
+    freelance: 'Available',
+    language: 'English, Bangla'
+
+
 }
 export default function PersonalDetails() {
     return (
         <div>
             <div>
-                <div class="relative">
-                    <Image src={person} alt="person" class="w-10 h-10 rounded-full" />
-                    <span class="bottom-0 left-7 absolute  w-3.5 h-3.5 bg-green-400 border-2 border-white dark:border-gray-800 rounded-full"></span>
+                <div class="relative w-fit mx-auto">
+                    <Image width={100} height={100} src={person} alt="person" class="rounded-full object-contain" />
+                    <span class="bottom-0 right-3 border-primary border-2 absolute  w-5 h-5 bg-green-500 rounded-full"></span>
                 </div>
             </div>
         </div>
