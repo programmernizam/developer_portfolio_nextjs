fb028f3ecc58c2b899f553093b3fc31cf4144d66
programmerNizam <mdnizam12109@gmail.com>
2024-02-13 00:33:12 +0600

    [Added] React Icons

--- src/components/LeftSidebar/PersonalDetails.js
+++ src/components/LeftSidebar/PersonalDetails.js
@@ -1,5 +1,18 @@
+import person from '@/assets/person.png';
+import Image from "next/image";
+
+const devInfo = {
+    name: 'MD Nizam Uddin'
+}
 export default function PersonalDetails() {
-  return (
-    <div>PersonalDetails</div>
-  )
+    return (
+        <div>
+            <div>
+                <div class="relative">
+                    <Image src={person} alt="person" class="w-10 h-10 rounded-full" />
+                    <span class="bottom-0 left-7 absolute  w-3.5 h-3.5 bg-green-400 border-2 border-white dark:border-gray-800 rounded-full"></span>
+                </div>
+            </div>
+        </div>
+    )
 }
