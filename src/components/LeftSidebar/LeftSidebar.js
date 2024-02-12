3a696cebe08ec0ba560165c10cd9939eb6f9a39a
programmerNizam <mdnizam12109@gmail.com>
2024-02-13 00:28:08 +0600

    [Created] Personal Details component

--- src/components/LeftSidebar/LeftSidebar.js
+++ src/components/LeftSidebar/LeftSidebar.js
@@ -1,5 +1,9 @@
+import PersonalDetails from "./PersonalDetails";
+
 export default function LeftSidebar() {
     return (
-        <div className="rounded-xl bg-base-100 p-6"></div>
+        <div className="rounded-xl bg-base-100 p-6">
+            <PersonalDetails />
+        </div>
     )
 }
---
+++ src/components/LeftSidebar/PersonalDetails.js
@@ -0,0 +1,5 @@
+export default function PersonalDetails() {
+  return (
+    <div>PersonalDetails</div>
+  )
+}
