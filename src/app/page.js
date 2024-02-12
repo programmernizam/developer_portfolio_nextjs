2c73192749f7d0a7e55dade05301c1e7dce6f9fc
programmerNizam <mdnizam12109@gmail.com>
2024-02-12 14:25:42 +0600

    [Building] Project layout setup

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,15 @@
 export default function Home() {
   return (
-    <div>Home</div>
+    <section className="max-w-[1470px] w-full mx-auto pt-8 relative">
+      <div className="flex gap-8">
+        <div className="max-w-[300px] w-full sticky top-5 z-10">
+          <div className="rounded-xl bg-base-100 p-6"></div>
+        </div>
+        <div className="w-full bg-base-100 p-6">
+
+        </div>
+        <div className="max-w-[88px] w-full border bg-base-100 p-6 sticky z-10 top-5"></div>
+      </div>
+    </section>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,35 +7,19 @@ module.exports = {
   ],
   theme: {
     extend: {
-      themes: [
-        {
-          light: {
-            "primary": "#FFB400",
-            "secondary": "#FFB400",
-            "heading": "#2B2B2B",
-            "paragraph": "#767676",
-            "base-100": "#E4E6EB",
-            "bg-100": "#FFFFFF",
-            "info": "#ffffff",
-            "success": "#00ffff",
-            "warning": "#ffffff",
-            "error": "#ffffff",
-          },
-          dark: {
-            "primary": "#0080ff",
-            "secondary": "#007696",
-            "accent": "#cd3f00",
-            "neutral": "#280b14",
-            "base-100": "#242c47",
-            "info": "#0067f5",
-            "success": "#64e400",
-            "warning": "#dca800",
-            "error": "#d32d48",
-          },
-        },
-      ],
-
-    },
+      colors: {
+        "primary": "#FFB400",
+        "secondary": "#FFB400",
+        "heading": "#2B2B2B",
+        "paragraph": "#767676",
+        "base-100": "#FFFFFF",
+        "base-200": "#E4E6EB",
+        "info": "#ffffff",
+        "success": "#00ffff",
+        "warning": "#ffffff",
+        "error": "#ffffff",
+      }
+    }
   },
   plugins: [],
 };
