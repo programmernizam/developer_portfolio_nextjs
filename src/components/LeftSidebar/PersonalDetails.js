be90db0b56e6ab69a1f5819e18172ac2a96fb311
programmerNizam <mdnizam12109@gmail.com>
2024-02-13 01:05:40 +0600

    [Added] Profile Socail Media icons and design

--- src/components/LeftSidebar/PersonalDetails.js
+++ src/components/LeftSidebar/PersonalDetails.js
@@ -1,5 +1,7 @@
 import person from '@/assets/profile.jpg';
 import Image from "next/image";
+import { AiFillInstagram } from "react-icons/ai";
+import { FaFacebookF, FaGithub, FaLinkedinIn, FaTwitter } from "react-icons/fa";
 
 const devInfo = {
     name: 'MD Nizam Uddin',
@@ -22,11 +24,23 @@ const devInfo = {
 export default function PersonalDetails() {
     return (
         <div>
-            <div>
-                <div class="relative w-fit mx-auto">
-                    <Image width={100} height={100} src={person} alt="person" class="rounded-full object-contain" />
-                    <span class="bottom-0 right-3 border-primary border-2 absolute  w-5 h-5 bg-green-500 rounded-full"></span>
-                </div>
+            {/* Profile Avatar Image */}
+            <div class="relative w-fit mx-auto">
+                <Image width={100} height={100} src={person} alt="person" class="rounded-full object-contain" />
+                <span class="bottom-0 right-3 border-primary border-2 absolute  w-5 h-5 bg-green-500 rounded-full"></span>
+            </div>
+            {/* Profile Info */}
+            <div className='my-5'>
+                <h2 className='text-xl font-semibold text-center'>{devInfo.name}</h2>
+                <p className=' mt-3 text-center'><span className='text-[14px] bg-secondary/15 inline-block px-5 text-primary rounded-full font-medium'>{devInfo.designation}</span></p>
+            </div>
+            {/* Social Media */}
+            <div className='flex items-center justify-center gap-4'>
+                <FaFacebookF className='text-paragraph text-[16px] hover:text-secondary cursor-pointer' />
+                <AiFillInstagram className='text-paragraph text-[16px] hover:text-secondary cursor-pointer' />
+                <FaTwitter className='text-paragraph text-[16px] hover:text-secondary cursor-pointer' />
+                <FaLinkedinIn className='text-paragraph text-[16px] hover:text-secondary cursor-pointer' />
+                <FaGithub className='text-paragraph text-[16px] hover:text-secondary cursor-pointer' />
             </div>
         </div>
     )
