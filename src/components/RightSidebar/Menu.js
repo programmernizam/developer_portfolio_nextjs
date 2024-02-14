dc5c8aff9f87abb924c6cde1c380dd2c604808c0
programmerNizam <mdnizam12109@gmail.com>
2024-02-14 17:12:44 +0600

    [Update] Menu Link and icon added dynamic

--- src/components/RightSidebar/Menu.js
+++ src/components/RightSidebar/Menu.js
@@ -1,34 +1,43 @@
+import Link from "next/link";
 import { FaBookOpen, FaBriefcase, FaFileAlt } from "react-icons/fa";
 import { HiHome, HiMiniCodeBracketSquare } from "react-icons/hi2";
 import { IoChatboxEllipses } from "react-icons/io5";
 
-const menu = [
+const menuItems = [
     {
         id: "home",
-    }
-]
+        icon: <HiHome />,
+    },
+    {
+        id: "code",
+        icon: <HiMiniCodeBracketSquare />,
+    },
+    {
+        id: "book",
+        icon: <FaBookOpen />,
+    },
+    {
+        id: "briefcase",
+        icon: <FaBriefcase />,
+    },
+    {
+        id: "file",
+        icon: <FaFileAlt />,
+    },
+    {
+        id: "chat",
+        icon: <IoChatboxEllipses />,
+    },
+];
 
 export default function Menu() {
     return (
         <div className="flex flex-col justify-center items-center gap-5">
-            <span className="p-4 hover:bg-primary rounded-lg">
-                <HiHome className="text-[24px] text-paragraph" />
-            </span>
-            <span className="p-4 hover:bg-primary rounded-lg">
-                <HiMiniCodeBracketSquare className="text-[24px] text-paragraph" />
-            </span>
-            <span className="p-4 hover:bg-primary rounded-lg">
-                <FaBookOpen className="text-[24px] text-paragraph" />
-            </span>
-            <span className="p-4 hover:bg-primary rounded-lg">
-                <FaBriefcase className="text-[24px] text-paragraph" />
-            </span>
-            <span className="p-4 hover:bg-primary rounded-lg">
-                <FaFileAlt className="text-[24px] text-paragraph" />
-            </span>
-            <span className="p-4 hover:bg-primary rounded-lg">
-                <IoChatboxEllipses className="text-[24px] text-paragraph" />
-            </span>
+            {menuItems.map((item) => (
+                <Link href={item.id} key={item.id} className="p-4 hover:bg-primary rounded-lg text-[24px] text-paragraph">
+                    {item.icon}
+                </Link>
+            ))}
         </div>
-    )
+    );
 }
