adcb77e7f0b39c6f17eac749b4ea93eb9482f239
programmerNizam <mdnizam12109@gmail.com>
2024-02-16 18:03:31 +0600

    [Added] Hero left side haeding styles

--- src/components/Main/Hero.js
+++ src/components/Main/Hero.js
@@ -7,7 +7,7 @@ export default function Hero() {
   return (
     <div className="max-h-[492px] bg-base-100 p-6 sm:p-10 xl:p-16 rounded-xl flex items-center justify-center md:justify-between">
       <div className='w-full md:max-w-[260px] lg:max-w-[460px]'>
-        <h1 className='text-5xl'>I design products that delight and inspire people.
+        <h1 className='text-3xl xl:text-5xl font-semibold text-center text-heading md:text-start leading-9 xl:leading-[56px]'>I design products that delight and inspire people.
         </h1>
         <p>A Product Designer and Visual Developer in SF. I specialize in UI/UX Design, Responsive Web Design, and Visual Development.</p>
         <button className='flex justify-center md:justify-start items-center bg-primary pl-6 py-1.5 rounded-full pr-1.5'>
