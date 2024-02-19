09fdb38c92fae5f7d5b5755ea0c063bf5671d597
programmerNizam <mdnizam12109@gmail.com>
2024-02-19 21:25:20 +0600

    [Created] Services data json

--- src/components/Main/Services.js
+++ src/components/Main/Services.js
@@ -4,7 +4,7 @@ export default function Services() {
             <h2 className="text-2xl md:text-3xl xl:text-5xl text-base-content font-bold text-center">My Services</h2>
             <p className="md:max-w-[438px] text-center text-base text-paragraph mx-auto mt-4">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. lorem ipsum</p>
             <div className="mt-10 md:mt-12 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 md:gap-6">
-
+                
             </div>
         </section>
     )
