039175f0d62f24e0f24fad0da2ba11902b221d53
programmerNizam <mdnizam12109@gmail.com>
2024-02-20 13:45:39 +0600

    [Done] Education top section

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,3 +1,4 @@
+import Education from "./Education";
 import Hero from "./Hero";
 import Services from "./Services";
 
@@ -6,6 +7,7 @@ export default function Main() {
         <div>
             <Hero />
             <Services />
+            <Education />
         </div>
     )
 }
