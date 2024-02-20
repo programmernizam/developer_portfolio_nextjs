048781359a0f69bbf5764ee57b6892c025ec3cb3
programmerNizam <mdnizam12109@gmail.com>
2024-02-20 20:12:50 +0600

    [Done] Education section

--- src/components/Main/Education.js
+++ src/components/Main/Education.js
@@ -7,7 +7,7 @@ export default function Education() {
             <h2 className="text-2xl md:text-3xl xl:text-5xl text-base-content font-bold text-center">Education</h2>
             <p className="md:max-w-[438px] text-center text-base text-paragraph mx-auto mt-4">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. lorem ipsum</p>
             <div className="p-6 md:p-10 bg-base-100 rounded-xl flex flex-col gap-8 md:gap-10 mt-10 xl:mt-12">
-                {education.map(edu => <EducationCard key={edu.id} edu={edu} index />)}
+                {education.map((edu, index) => <EducationCard key={edu.id} edu={edu} />)}
             </div>
         </div>
     )
--- src/components/Main/EducationCard.js
+++ src/components/Main/EducationCard.js
@@ -1,7 +1,7 @@
-export default function EducationCard({ edu, index }) {
+export default function EducationCard({ edu }) {
     const { title, university, year, description } = edu
     return (
-        <div className={`${index}flex flex-col gap-6 xl:flex-row xl:justify-between`}>
+        <div className={`flex flex-col gap-6 xl:flex-row xl:justify-between ${edu.id === "002" ? "py-8 xl:py-10 border-t border-b border-gray-300/50" : "border-none"}`}>
             <div className="pl-3 xl:pl-0 border-l-2 xl:border-l-0 border-paragraph/10 w-full xl:max-w-[315px]">
                 <h3 className="text-lg text-heading font-semibold mb-2 xl:mb-3">{university}</h3>
                 <p className="text-xs font-medium text-primary px-3 py-1 bg-primary/10 w-fit rounded-md">{year}</p>
