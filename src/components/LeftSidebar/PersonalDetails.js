f1aa76535ada0a67317b804c0cf1bb67c87cbb30
programmerNizam <mdnizam12109@gmail.com>
2024-02-21 00:46:24 +0600

    [Update] Personal Details Designation style

--- src/components/LeftSidebar/PersonalDetails.js
+++ src/components/LeftSidebar/PersonalDetails.js
@@ -9,12 +9,12 @@ export default function PersonalDetails({ data }) {
             {/* Profile Avatar Image */}
             <div className="relative w-fit mx-auto">
                 <Image width={100} height={100} src={person} alt="person" className="rounded-full object-contain" />
-                <span className="bottom-0 right-3 border-primary border-2 absolute  w-5 h-5 bg-green-500 rounded-full"></span>
+                <span className="bottom-0 right-3 absolute  w-5 h-5 bg-primary rounded-full"></span>
             </div>
             {/* Profile Info */}
             <div className='my-5'>
                 <h2 className='text-xl font-semibold text-center'>{data.name}</h2>
-                <p className=' mt-3 text-center'><span className='text-[14px] bg-secondary/15 inline-block px-5 text-primary rounded-full font-medium'>{data.designation}</span></p>
+                <p className=' mt-3 text-center'><span className='text-xs font-medium text-primary px-3 py-1 bg-primary/10 w-fit rounded-md'>{data.designation}</span></p>
             </div>
             {/* Social Media */}
             <div className='flex items-center justify-center gap-5'>
