462a2f7f501a66dc36e96ff4d50155fb96ffa943
programmerNizam <mdnizam12109@gmail.com>
2024-02-21 16:58:31 +0600

    [Start] Blog section

---
+++ src/components/Main/Blogs.js
@@ -0,0 +1,10 @@
+import CommonTitle from "./CommonTitle";
+
+export default function Blogs() {
+    return (
+        <section>
+            <CommonTitle title={"Latest Blog"} description={"Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. lorem ipsum"} />
+            <div></div>
+        </section>
+    )
+}
