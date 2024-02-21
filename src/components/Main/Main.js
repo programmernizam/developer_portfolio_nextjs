c64f8c6ea6a1731212dde3c2213eca7100f69565
programmerNizam <mdnizam12109@gmail.com>
2024-02-21 12:16:59 +0600

    [Start] Testimonial section

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,6 +3,7 @@ import Experience from "./Experience";
 import Hero from "./Hero";
 import Portfolio from "./Portfolio";
 import Services from "./Services";
+import Testimonial from "./Testimonial";
 
 export default function Main() {
     return (
@@ -12,6 +13,7 @@ export default function Main() {
             <Education />
             <Experience />
             <Portfolio />
+            <Testimonial />
         </div>
     )
 }
---
+++ src/components/Main/Testimonial.js
@@ -0,0 +1,12 @@
+import CommonTitle from "./CommonTitle";
+
+export default function Testimonial() {
+    return (
+        <section>
+            <CommonTitle title={"Testimonials"} description={"Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. lorem ipsum"} />
+            <div className="mt-10 xl:mt-12">
+                
+            </div>
+        </section>
+    )
+}
