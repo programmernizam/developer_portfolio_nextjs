3da36473d81aec1a2ab0039af30cd5f19ad7fc23
programmerNizam <mdnizam12109@gmail.com>
2024-02-23 19:20:19 +0600

    [Done] Blog Card top section

--- src/components/Main/BlogCard.js
+++ src/components/Main/BlogCard.js
@@ -1,6 +1,18 @@
+import Image from "next/image";
+import Link from "next/link";
 
-export default function BlogCard({ title, description, img }) {
+export default function BlogCard({ blog }) {
+    const { title, description, img } = blog;
     return (
-        <div className="bg-base-100 rounded-xl">BlogCard</div>
+        <div className="bg-base-100 rounded-xl">
+            <Image className="w-full rounded-xl" width={324} height={216} src={img} alt="title" />
+            <div className="p-6">
+                <h3 className="text-lg font-semibold text-heading hover:text-primary hover:duration-300 transition">{title}</h3>
+                <p className="mt-2 mb-4 text-base text-paragraph">{description}</p>
+                <Link href={"#"} className="text-primary text-sm font-semibold flex items-center gap-3 hover:opacity-80 transition hover:duration-300">
+                    Read More
+                </Link>
+            </div>
+        </div>
     )
 }
