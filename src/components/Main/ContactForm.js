e70eaf020e8a02e06aef7513ff7c0f1f15036a01
programmerNizam <mdnizam12109@gmail.com>
2024-02-24 12:39:56 +0600

    [Done] Contact form name and email filed

--- src/components/Main/ContactForm.js
+++ src/components/Main/ContactForm.js
@@ -3,7 +3,16 @@ export default function ContactForm() {
         <div className="lg:col-span-5 bg-base-100 p-6 lg:p-10 rounded-xl w-full">
             <h3 className="text-xl font-bold pb-6">Leave a Message</h3>
             <form action="">
-                <div className="flex flex-wrap lg:flex-nowrap items-center gap-6"></div>
+                <div className="flex flex-wrap lg:flex-nowrap items-center gap-6">
+                    <div className="w-full">
+                        <p className="text-sm text-paragraph pb-2">Name</p>
+                        <input type="text" className="w-full bg-base-200 rounded-lg outline-none px-4 h-12" />
+                    </div>
+                    <div className="w-full">
+                        <p className="text-sm text-paragraph pb-2">Email</p>
+                        <input type="email" className="w-full bg-base-200 rounded-lg outline-none px-4 h-12" />
+                    </div>
+                </div>
             </form>
         </div>
     )
