4380863ca75fd91f6a138169268c88322db5dfa2
programmerNizam <mdnizam12109@gmail.com>
2024-02-24 12:37:48 +0600

    [Start] Building Contact us form component

--- src/components/Main/Contact.js
+++ src/components/Main/Contact.js
@@ -1,9 +1,13 @@
 import CommonTitle from "./CommonTitle";
+import ContactForm from "./ContactForm";
 
 export default function Contact() {
     return (
         <section id="contact" className="mb-16 mb:mt-24 xl:mb-28">
-            <CommonTitle title={"Get In Touch"} />
+            <CommonTitle title={"Get In Touch"} description={"Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. lorem ipsum"} />
+            <div className="flex flex-wrap md:flex-nowrap lg:grid lg:grid-cols-7 md:gap-6 mt-12">
+                <ContactForm />
+            </div>
         </section>
     )
 }
---
+++ src/components/Main/ContactForm.js
@@ -0,0 +1,10 @@
+export default function ContactForm() {
+    return (
+        <div className="lg:col-span-5 bg-base-100 p-6 lg:p-10 rounded-xl w-full">
+            <h3 className="text-xl font-bold pb-6">Leave a Message</h3>
+            <form action="">
+                <div className="flex flex-wrap lg:flex-nowrap items-center gap-6"></div>
+            </form>
+        </div>
+    )
+}
