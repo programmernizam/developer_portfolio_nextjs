0598bb571673bc3001be7ebe9ed991824905e73c
programmerNizam <mdnizam12109@gmail.com>
2024-02-29 01:57:55 +0600

    [Update] Call contact info card in contact info

--- src/components/Main/ContactInfo.js
+++ src/components/Main/ContactInfo.js
@@ -1,7 +1,11 @@
+import ContactInfoCard from "./ContactInfoCard";
+
 export default function ContactInfo() {
     return (
         <div className="lg:col-span-2 space-y-6 mt-6 md:mt-0 w-full">
-            
+            <ContactInfoCard />
+            <ContactInfoCard />
+            <ContactInfoCard />
         </div>
     )
 }
