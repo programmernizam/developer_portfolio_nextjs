cafc5e079817e3319b66934cf2773dee5113a4f6
programmerNizam <mdnizam12109@gmail.com>
2024-02-29 02:17:01 +0600

    [Create] Dynamic year function

--- src/components/Main/Footer.js
+++ src/components/Main/Footer.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 
 export default function Footer() {
+    const year = new Date().getFullYear();
     return (
         <footer className='p-5 bg-base-100 rounded-xl container mx-auto text-base text-paragraph text-center mb-8'>
             © 2023 <Link href={"https://www.linkedin.com/in/programmernizam/"}>ProgrammerNizam</Link>. All Rights Reserved.
