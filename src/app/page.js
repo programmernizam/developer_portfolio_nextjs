8d90af422f8f03c4f17b0ccf09459c7f5ee98614
programmerNizam <mdnizam12109@gmail.com>
2024-03-04 15:55:20 +0600

    [Added] Responsive main section

--- src/app/page.js
+++ src/app/page.js
@@ -6,13 +6,13 @@ export default function Home() {
   return (
     <section className="max-w-[1470px] w-full mx-auto pt-8 relative">
       <div className="flex gap-8">
-        <div className="max-w-[300px] w-full sticky top-5 z-10  h-[calc(100vh-2rem)]">
+        <div className="max-w-[300px] w-full sticky top-5 z-10  h-[calc(100vh-2rem)] hidden xl:inline-block">
           <LeftSidebar />
         </div>
         <div className="w-full">
           <Main />
         </div>
-        <div className="min-w-[88px] h-[calc(100vh-2rem)] bg-base-100 py-5 rounded-xl scrollbarStyle sticky top-5 mb-10 z-10">
+        <div className="min-w-[88px] h-[calc(100vh-2rem)] bg-base-100 py-5 rounded-xl scrollbarStyle sticky top-5 mb-10 z-10 hidden xl:inline-block">
           <RightSidebar />
         </div>
       </div>
